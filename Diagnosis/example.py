1e08e31d92d0ea44af5251a7d5a0afdd4b9a4aed
Wenliang <wmz5132@psu.edu>
2025-04-11 18:57:37 -0400

    update example

--- Diagnosis/example.py
+++ Diagnosis/example.py
@@ -1,7 +1,7 @@
 # python code to set env variables for huggingface download directory
-import os
-os.environ['HF_HOME'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
-os.environ['HF_DATASETS_CACHE'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
+# import os
+# os.environ['HF_HOME'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
+# os.environ['HF_DATASETS_CACHE'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
 
 from models import GPT, Llama32
 from tester import DiagnosisTester
@@ -9,8 +9,8 @@ import torch
 
 
 # Example 1: Run 150 complexgrid_3x3 samples on local model
-model = Llama32(model_path="/scratch1/wmz5132/models/huggingface/Llama-3.2-11B-Vision-Instruct", torch_dtype=torch.bfloat16, device_map="cuda:1")
-tester = DiagnosisTester(model=model, dataset_name="complexgrid_3x3", num_samples=10)
+model = Llama32(model_path="meta-llama/Llama-3.2-11B-Vision-Instruct", torch_dtype=torch.bfloat16, device_map="cuda")
+tester = DiagnosisTester(model=model, dataset_name="complexgrid_3x3", num_samples=150)
 # feel free to set generation kwargs here or leave it empty
 tester.run(max_new_tokens=100, temperature=1e-5)
 tester.eval()
--- RealWorld/example.py
+++ RealWorld/example.py
@@ -1,7 +1,7 @@
 # python code to set env variables for huggingface download directory
-import os
-os.environ['HF_HOME'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
-os.environ['HF_DATASETS_CACHE'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
+# import os
+# os.environ['HF_HOME'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
+# os.environ['HF_DATASETS_CACHE'] = '/scratch1/wmz5132/HRScenne/data/huggingface'
 
 from models import GPT, Qwen2VL
 from tester import RealWorldTester
@@ -16,7 +16,7 @@ tester.eval()
 
 # example 2: use local Qwen2VL model to run full realworld dataset
 model = Qwen2VL(
-    model_path="/scratch1/wmz5132/models/huggingface/Qwen2-VL-7B-Instruct",
+    model_path="Qwen/Qwen2-VL-7B-Instruct",
     torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2", device_map="cuda"
 )
 tester = RealWorldTester(model=model, dataset_name="realworld_combined", split="test")
